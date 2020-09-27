be76a8a38e9791a1f002f7285287e388d919d3dd
RileyBarrett7 <63364979+RileyBarrett7@users.noreply.github.com>
2020-09-27 18:17:21 -0400

    added more gsap

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -5,7 +5,8 @@ import {gsap} from "gsap";
     Demo Script
 ----------------- */
 
-var timeOfAnimation = 3;
+var timeOfAnimation = 2;
 
-gsap.to(".red-box",{duration: timeOfAnimation,borderRadius: "20"});
-gsap.to(".red-box",{duration: timeOfAnimation, rotation:360, repeat:3, delay:timeOfAnimation });
+gsap.to(".blue-box",{duration: timeOfAnimation,borderRadius: "100", y:500, x:100});
+gsap.to(".black-rectangle",{duration: 10, alpha:0,  x:500, scale:.5});
+gsap.to(".red-box",{duration: 2, x:100, Y:50, rotation: 300})
