5af5f69a3c7e6d4d97fdeb24a3cbae5f38a37a0d
RileyBarrett7 <63364979+RileyBarrett7@users.noreply.github.com>
2020-10-19 01:17:44 -0400

    fixed html problem

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -5,8 +5,8 @@ import {gsap} from "gsap";
     Demo Script
 ----------------- */
 
-var timeOfAnimation = 2;
+// var timeOfAnimation = 2;
 
-gsap.to(".blue-box",{duration: timeOfAnimation,borderRadius: "100", y:500, x:100});
-gsap.to(".black-rectangle",{duration: 10, alpha:0,  x:500, scale:.5});
-gsap.to(".red-box",{duration: 2, x:100, Y:50, rotation: 300})
+// gsap.to(".blue-box",{duration: timeOfAnimation,borderRadius: "100", y:500, x:100});
+// gsap.to(".black-rectangle",{duration: 10, alpha:0,  x:500, scale:.5});
+// gsap.to(".red-box",{duration: 2, x:100, Y:50, rotation: 300})
