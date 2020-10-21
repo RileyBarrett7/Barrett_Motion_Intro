20302f52850e719bbffff76d36336469d5623e4c
RileyBarrett7 <63364979+RileyBarrett7@users.noreply.github.com>
2020-10-20 20:42:34 -0400

    added js for load bar

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,5 +1,38 @@
 import {gsap} from "gsap";
 
+// MorphSVGPlugin.convertToPath("circle");
+
+const saveButtonTL = gsap.timeline();
+saveButtonTL.from("#load-bar",{duration:5, x:-110});
+
+// const MorphSVGPlugin = gsap.timeline();
+
+// var tl = gsap.timeline({repeat:20, repeatDelay:0.3, yoyo:true, defaults:{duration:1}})
+
+// tl.to("#circle", {morphSVG:"#c"})
+//   .timeScale(3)
+
+
+export function saveButtonAnimation(){
+    return saveButtonTL;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* -----------------
     Demo Script
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,15 @@
-import * as Demo from './demo.js';
+// import * as Demo from './demo.js';
 
-console.log(Demo);
+// console.log(Demo);
+
+import {gsap} from "gsap";
+
+import {saveButtonAnimation} from "./demo.js";
+
+// import {MorphSVGPlugin} from "./demo.js";
+
+const mainTl = gsap.timeline();
+
+mainTl.add(saveButtonAnimation());
+
+// mainTl.add(MorphSVGPlugin());
