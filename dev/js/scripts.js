e263f158907a9f601af3285e662d751bd848e105
RileyBarrett7 <63364979+RileyBarrett7@users.noreply.github.com>
2020-11-19 12:21:39 -0500

    changed timing

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,11 +1,9 @@
-// import * as Demo from './demo.js';
-
-// console.log(Demo);
-
 import {gsap} from "gsap";
-
+// import {GSDevTools} from "gsap/GSDevTools";
 // import {logoAnimation} from "./demo.js";
 import {loadBarAnimation} from "./demo.js";
+
+// gsap.registerPlugin(GSDevTools);
 
 
 const mainTl = gsap.timeline();
@@ -13,3 +11,4 @@ const mainTl = gsap.timeline();
 // mainTl.add(logoAnimation());
 mainTl.add(loadBarAnimation());
 
+// GSDevTools.create();
