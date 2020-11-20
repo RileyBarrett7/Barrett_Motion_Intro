4495c42691ecc1d344141ae1ae51835f25ef870c
RileyBarrett7 <63364979+RileyBarrett7@users.noreply.github.com>
2020-11-19 20:20:09 -0500

    added more to loadscreen

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -34,8 +34,34 @@ countIt();
 
 // LOADING BAR ANIMATION
 const loadBarTL = gsap.timeline();
-loadBarTL.from("#loadbar-left",{duration:3, x:157, ease: "power4.out"}, "sametime")
-          .from("#loadbar-right",{duration:3, x:-157, ease: "power4.out"}, "sametime");
+loadBarTL.from("#logo",{duration:1, scale:350, x:-11000, y:-3800})
+          .from("#light-right-cp",{duration:1, alpha:0}, "sametime4")
+          .from("#light-right",{duration:1, alpha:0}, "sametime4")
+          .from("#loadbar-right",{duration:1,  alpha:0}, "sametime4")
+          .from("#light-left-cp",{duration:1, alpha:0}, "sametime4")
+          .from("#light-left",{duration:1, alpha:0}, "sametime4")
+          .from("#loadbar-left",{duration:1, alpha:0}, "sametime4")
+
+          .from("#counter",{duration:1, alpha:0}, "sametime5")
+          .from("#percentage",{duration:1, alpha:0}, "sametime5")
+          .from("#loading-text",{duration:1, alpha:0}, "sametime5")
+
+          .from("#loadbar-left",{duration:3, x:157, ease: "power3.out"}, "sametime")
+          .from("#loadbar-right",{duration:3, x:-157, ease: "power3.out"}, "sametime")
+
+          .to("#counter",{duration:1, alpha:0}, "sametime2")
+          .to("#percentage",{duration:1, alpha:0}, "sametime2")
+          .to("#loading-text",{duration:1, alpha:0}, "sametime2")
+
+          .to("#light-right-cp",{duration:1, x:500, alpha:0}, "sametime3")
+          .to("#light-right",{duration:1, x:500, alpha:0}, "sametime3")
+          .to("#loadbar-right",{duration:1, x:500, alpha:0}, "sametime3")
+          .to("#light-left-cp",{duration:1, x:-500, alpha:0}, "sametime3")
+          .to("#light-left",{duration:1, x:-500, alpha:0}, "sametime3")
+          .to("#loadbar-left",{duration:1, x:-500, alpha:0}, "sametime3")
+          // .to("#logo",{duration:5, scale:350, x:-11000, y:-3800, ease: CustomEase.create("custom", "M0,0,C0.336,0.066,0.972,0.026,1,1")});
+          .to("#logo",{duration:3, scale:350, x:-11000, y:-3800});
+         
 
 
 
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,9 +1,11 @@
 import {gsap} from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
+// import { CustomeEase } from "gsap/GSDevTools";
 // import { logoAnimation } from "./demo.js";
 import {loadBarAnimation} from "./demo.js";
 
 gsap.registerPlugin(GSDevTools);
+// gsap.registerPlugin(CustomeEase);
 
 
 const mainTl = gsap.timeline();
@@ -12,3 +14,4 @@ const mainTl = gsap.timeline();
 mainTl.add(loadBarAnimation());
 
 GSDevTools.create();
+// CustomeEase.create();
