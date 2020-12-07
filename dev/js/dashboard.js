11b5ac33697035c355f69194352dc265f6eadd2a
RileyBarrett7 <63364979+RileyBarrett7@users.noreply.github.com>
2020-12-07 16:55:09 -0500

    added road

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -15,12 +15,17 @@ dashBoardTL.from("#dashboard-full",{duration:.5, alpha:0}, "sametime3")
             // .from("#speedometer",{duration:1, alpha:0, ease: "power4.out"}, "sametime2")
 
 
-            // SPEEDOMETER DRAWSVG
-
-            .from("#speed-back-stroke",{duration:.5, drawSVG:"50%"})
+            // BACK SPEEDOMETER DRAW IN
+            .from("#speed-back-stroke",{duration:.5, drawSVG:"15% 40%"})
             .from("#rpm-back-stroke",{duration:.5, drawSVG:"80% 30%"})
-            
 
+            // ROAD DRAWING IN
+            .from("#line-left",{duration:.5, drawSVG:"0%"}, "sametime2")
+            .from("#line-right",{duration:.5, drawSVG:"0%"}, "sametime2")
+            .from("#arrow",{duration:.5, alpha:0})
+            .from("#line-middle",{duration:.5, drawSVG:"0%"})
+
+            // ELEMENTS FADE IN DRAWSVG
             .from("#park-drive",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
             .from("#R8",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
             .from("#RPM",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
@@ -29,11 +34,16 @@ dashBoardTL.from("#dashboard-full",{duration:.5, alpha:0}, "sametime3")
             .from("#speed-numbers",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
             .from("#rpm-numbers",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
             .from("#mph",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
-            .from("#road",{duration:1, alpha:0, ease: "power4.out"})
+
 
             .from("#side-icons",{duration:.3, x:1200}, "sametime4")
             .from("#music",{duration:.3, y:800}, "sametime4")
 
+            // ROAD DRAWING OUT
+            .to("#line-left",{duration:.5, drawSVG:"0%"}, "sametime5")
+            .to("#line-right",{duration:.5, drawSVG:"0%"}, "sametime5") 
+            .to("#arrow",{duration:.5, alpha:0}, "sametime5")
+            .to("#line-middle",{duration:.5, drawSVG:"0%"}, "sametime5")
             
             .to("#fan-full-icon",{duration:.5, x:11})
             .from("#temp",{duration:.5, x:275})
