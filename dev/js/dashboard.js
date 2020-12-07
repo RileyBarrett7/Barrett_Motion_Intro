a59d9c763a182f892f66487c9b7614101634387c
RileyBarrett7 <63364979+RileyBarrett7@users.noreply.github.com>
2020-12-07 16:23:13 -0500

    started draw svg

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -1,5 +1,7 @@
 import {gsap} from "gsap";
+import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
 
+gsap.registerPlugin(DrawSVGPlugin);
 
 
 
@@ -11,12 +13,26 @@ dashBoardTL.from("#dashboard-full",{duration:.5, alpha:0}, "sametime3")
             .from("#Media",{duration:.5, alpha:0}, "sametime3")
 
             // .from("#speedometer",{duration:1, alpha:0, ease: "power4.out"}, "sametime2")
-            .from("#speed",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
-            .from("#mph",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
-            .from("#road",{duration:1, alpha:0, ease: "power4.out"}, "sametime2")
 
-            .from("#side-icons",{duration:1, x:1100}, "sametime")
-            .from("#music",{duration:1, y:1000}, "sametime")
+
+            // SPEEDOMETER DRAWSVG
+
+            .from("#speed-back-stroke",{duration:.5, drawSVG:"50%"})
+            .from("#rpm-back-stroke",{duration:.5, drawSVG:"80% 30%"})
+            
+
+            .from("#park-drive",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
+            .from("#R8",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
+            .from("#RPM",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
+            .from("#x1000",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
+            .from("#speed",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
+            .from("#speed-numbers",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
+            .from("#rpm-numbers",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
+            .from("#mph",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
+            .from("#road",{duration:1, alpha:0, ease: "power4.out"})
+
+            .from("#side-icons",{duration:.3, x:1200}, "sametime4")
+            .from("#music",{duration:.3, y:800}, "sametime4")
 
             
             .to("#fan-full-icon",{duration:.5, x:11})
@@ -31,12 +47,10 @@ dashBoardTL.from("#dashboard-full",{duration:.5, alpha:0}, "sametime3")
 
             .to("#battery-full-icon",{duration:.5, x:11})
             .from("#car-icons",{duration:.5, x:275})
-            .to("#battery-side-icon",{duration:.5, x:-8});
+            .to("#battery-side-icon",{duration:.5, x:-8})
 
-
-
-
-
+            .from("#rpm-red-stroke",{duration:2, drawSVG:"0%"})
+            .from("#speed-red-stroke",{duration:2, drawSVG:"0%"});
 
 
 export function dashBoardAnimation(){
