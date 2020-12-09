d9612c03c971751a14285db309d95a040aa71776
RileyBarrett7 <63364979+RileyBarrett7@users.noreply.github.com>
2020-12-09 13:18:36 -0500

    added countIt code

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -13,12 +13,12 @@ gsap.registerPlugin(DrawSVGPlugin);
 // 500 = 1/2 second
 // 250 = 1/4 second
 // */
-// var counterSpeed = 1000;
+// var counterSpeed = 6000;
 
 // // Set this number you want your counter to count up to. Default is 20
 // var topSpeed = 86;
 
-// // If yu don't want your speed to start at 0, change it here. Default is 0.
+// // If you don't want your speed to start at 0, change it here. Default is 0.
 // var speedNumber = 0;
 
 // var myVar = setInterval(speedCounter, counterSpeed);
@@ -33,6 +33,22 @@ gsap.registerPlugin(DrawSVGPlugin);
 // 	return speedNumber;
 // }
 
+var startCount = 0,
+    endCount,
+    activeIndex = 1,
+    tm = 6,
+    num = { var: startCount },
+    numbers = document.getElementById("counter");
+    
+
+function countIt() {
+activeIndex == 1 ? endCount = 98 : endCount = 98;
+gsap.to(num, tm, {var: endCount, onUpdate:changeNumber});
+}
+
+function changeNumber() {
+  numbers.innerHTML = (num.var).toFixed();  
+}
 
 
 const dashBoardTL = gsap.timeline();
@@ -64,14 +80,19 @@ dashBoardTL.from("#dashboard-full",{duration:.5, alpha:0}, "sametime3")
             .from("#speed-numbers",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
             .from("#rpm-numbers",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
             .from("#mph",{duration:1, alpha:0, ease: "power4.out"}, "sametime4")
+    
             .from("#side-icons",{duration:.3, x:1200}, "sametime4")
             .from("#music",{duration:.3, y:800}, "sametime4")
+            .from("#album-cover",{duration:.5, alpha:0, ease: "none"}, "sametime4")
+
+            .from("#speed-counter",{duration:1, alpha:0, onComplete: countIt}, "sametime4")
 
             // SPEEDOMETER DRIVING
             .to("#D",{duration:.3, fill: "#F50437"}, "sametime7")
             .to("#P",{duration:.3, fill: "#FFF"}, "sametime7")
 
-            .from("#speed-red-stroke",{duration:4, drawSVG:"0%", ease: "power4.out"})
+            // .from("#speed-red-stroke",{duration:4, drawSVG:"0%", ease: "power4.out"})
+            .fromTo("#speed-red-stroke",{drawSVG:"0%"}, {duration: 6, drawSVG:"40%", ease: "power2.out"}, "play13-1")
             // .to("#speed-red-stroke",{duration:2, drawSVG:"60%"})
 
 
